069ef1e4e254e1e5e42c0e6387e71d3e85c2ef3c
nesayd <62736443+nesayd@users.noreply.github.com>
2022-06-27 09:01:37 -0700

    Update SEC.py

--- SEC.py
+++ SEC.py
@@ -314,25 +314,6 @@ def set_logger(path, logging_level):
         os.makedirs("Logs")
 
     logger = logging.getLogger(__name__)
-    logging.basicConfig(level=logging_level, format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%a, %d %b %Y %H:%M:%S', filename=f'{path}/ETL.log', filemode='a')    
-    
-
-    
-url = "https://www.sec.gov/dera/data/financial-statement-and-notes-data-set.html"
-download_path = './zip_files'
-text_file_path = './uploaded_file_list.txt'
-unzip_path = './unzip_files'
-dataset_path = './datasets'
-log_path = './Logs'
-
-print(f"Script started. Check log file details: {log_path}")
-
-start = time.time()
-set_logger(log_path, logging.INFO)
-links = get_links(url)
-download_zipfiles(download_path, links)
-unzip(unzip_path, download_path)
-upload_append_csv_files(text_file_path=text_file_path, unzip_path=unzip_path,
-                        dataset_path=dataset_path, dataset_name='SEC_XBRL', username='XXX')
-
-print(f"The whole script runs in: {time.time() - start} seconds")
+    logging.basicConfig(level=logging_level, format='%(asctime)s %(levelname)-8s %(message)s',
+                        datefmt='%a, %d %b %Y %H:%M:%S', filename=f'{path}/ETL.log', filemode='a')
+ 
